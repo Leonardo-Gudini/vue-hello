51a8819a83ef2ceb94f8ce3017669a615a5ca2c5
Leonardo Gudini <leonardo.gudini98@gmail.com>
2022-07-23 12:47:09 +0200

    aggiunto messaggio e immagine da data

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var container = new Vue({
+
+    el: "#container",
+
+    data:{
+        saluto: "Hello World!",
+        image: "https://docs.microsoft.com/it-it/shows/hello-world/media/helloworld_383x215.png"
+    }
+});
